a73f15fd1ab9a60eedf500fbc6d5a3b421aa0a45
mr-sazzad <main.sazzad@gmail.com>
2023-11-21 20:29:30 +0600

    some modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="text-gray-800 py-12 border-t border-gray-300">
+    <footer className="text-gray-800 py-12 border-t border-gray-300 mt-10">
       <div className="container mx-auto flex flex-col items-center">
         <h2 className="text-3xl font-bold mb-4">E-Commerce</h2>
         <div className="flex space-x-4 mb-8">
