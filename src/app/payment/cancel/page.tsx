63655ff788c4f35e7cdbde4ae7ef2965eca6660c
mr-sazzad <main.sazzad@gmail.com>
2023-11-22 06:27:05 +0600

    cancel page modified

--- src/app/payment/cancel/page.tsx
+++ src/app/payment/cancel/page.tsx
@@ -1,9 +1,57 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
+import { SiAmazoncloudwatch } from "react-icons/si";
+import { HiMiniArrowSmallRight } from "react-icons/hi2";
+
 const PaymentCancel = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Payment Cancel</h1>
+    <div className="max-w-[1300px] mx-auto">
+      <div className="flex justify-center items-center h-[100vh] w-full">
+        <div>
+          <div></div>
+          <div className="flex flex-col items-center">
+            <div>
+              <p className="font-semibold text-gray-500 flex gap-1 items-center">
+                <SiAmazoncloudwatch />
+                SORRY
+              </p>
+              <p className="text-2xl font-bold text-gray-600">Payment Cancel</p>
+            </div>
+            <button
+              className="
+                mt-5 
+                border 
+                border-rose-500 
+                text-rose-500 
+                hover:bg-rose-500 
+                hover:text-white 
+                px-4 py-[5px] 
+                font-bold transition 
+                duration-300
+                flex
+                gap-1
+                items-center
+              "
+              onClick={() => router.push("/")}
+            >
+              Go To Home Page{" "}
+              <HiMiniArrowSmallRight
+                className="animate-bounce"
+                style={{
+                  animationDuration: "1.5s",
+                  animationIterationCount: "infinite",
+                  animationTimingFunction: "ease",
+                }}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
