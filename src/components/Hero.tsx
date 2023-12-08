1188f53460cadf916a06cd2c652ca2357f5968a0
mr-sazzad <main.sazzad@gmail.com>
2023-12-08 13:03:20 +0600

    some modified on hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Link from "next/link";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import Link from "next/link";
 
 const HeroSection = () => {
   return (
