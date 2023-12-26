4402526e7bea3e18485840e52a62e6fe925ef120
mr-sazzad <main.sazzad@gmail.com>
2023-12-26 15:13:12 +0600

    some modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/Hero";
-// import LatestReviews from "@/components/LatestReviews";
-// import Promotion from "@/components/Promotion";
+import LatestReviews from "@/components/LatestReviews";
+import Promotion from "@/components/Promotion";
 // import Recent from "@/components/Recent";
 // import Upcoming from "@/components/Upcoming";
 
@@ -11,7 +11,7 @@ export default function Home() {
       {/* <Recent /> */}
       {/* <Promotion /> */}
       {/* <Upcoming /> */}
-      {/* <LatestReviews /> */}
+      <LatestReviews />
     </div>
   );
 }
