93b0b364a29e1446404274f4daca07c6d7b5c4d2
mr-sazzad <main.sazzad@gmail.com>
2023-12-26 15:05:41 +0600

    some modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import HeroSection from "@/components/Hero";
 // import LatestReviews from "@/components/LatestReviews";
 import Promotion from "@/components/Promotion";
-import Recent from "@/components/Recent";
+// import Recent from "@/components/Recent";
 import Upcoming from "@/components/Upcoming";
 
 export default function Home() {
   return (
     <div className="max-w-[1200px] mx-auto">
       <HeroSection />
-      <Recent />
+      {/* <Recent /> */}
       <Promotion />
       <Upcoming />
       {/* <LatestReviews /> */}
